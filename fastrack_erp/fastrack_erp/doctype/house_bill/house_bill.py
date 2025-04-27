5f381c74809c113cbf592db641f4b8d61e5f8cc6
sohan653 <sohanurl653@gmail.com>
2025-04-27 08:06:00 +0000

    modified fastrack item

--- fastrack_erp/fastrack_erp/doctype/house_bill/house_bill.py
+++ fastrack_erp/fastrack_erp/doctype/house_bill/house_bill.py
@@ -15,7 +15,7 @@ class HouseBill(Document):
 		master_bill_no = self.master_bill_no
 		house_bill_no = self.name
 		for item in self.container_items:
-			get_master_item = get_single_fastrack_item_by_bill_no([master_bill_no], item.marks_and_nos_container_and_seals)
+			get_master_item = get_single_fastrack_item_by_bill_no([master_bill_no], item.container_no)
 			get_existing_house_doc_id = frappe.db.get_list('House Bill', {'master_bill_no': master_bill_no,'docstatus': 1}, ['name'])
 			get_existing_house_doc_id = [doc['name'] for doc in get_existing_house_doc_id]
 			
@@ -23,12 +23,9 @@ class HouseBill(Document):
 			if self.name in get_existing_house_doc_id:
 				get_existing_house_doc_id.remove(self.name)
 			
-			get_house_item = get_single_fastrack_item_by_bill_no(get_existing_house_doc_id, item.marks_and_nos_container_and_seals, parent_type='House Bill')
-			print(get_master_item['gross_weight_cargo'])
-			print(get_house_item['gross_weight_cargo'])
-			print(item.gross_weight_cargo)
+			get_house_item = get_single_fastrack_item_by_bill_no(get_existing_house_doc_id, item.container_no, parent_type='House Bill')
 			if get_master_item['gross_weight_cargo'] < get_house_item['gross_weight_cargo'] + item.gross_weight_cargo:
-				frappe.throw(f"Weight mismatch for item: {item.marks_and_nos_container_and_seals}, should be less than or equal to {get_master_item['gross_weight_cargo']}")
+				frappe.throw(f"Weight mismatch for container: {item.container_no}, should be less than or equal to {get_master_item['gross_weight_cargo']}")
 
 @frappe.whitelist()
 def get_single_fastrack_item_by_bill_no(bill_no, item_name, parent_type='Master Bill'):
@@ -44,13 +41,13 @@ def get_single_fastrack_item_by_bill_no(bill_no, item_name, parent_type='Master 
 	# Build the query with properly formatted bill_no
 	result = frappe.db.sql(f"""
 		SELECT 
-			marks_and_nos_container_and_seals as item_name,
+			container_no as item_name,
 			SUM(gross_weight_cargo) as gross_weight_cargo
 		FROM `tabFastrack Item`
 		WHERE parenttype = '{parent_type}'
 		AND parent IN ({bill_no_placeholder})
-		AND marks_and_nos_container_and_seals = '{item_name}'
-		GROUP BY marks_and_nos_container_and_seals
+		AND container_no = '{item_name}'
+		GROUP BY container_no
 	""", as_dict=True)
 	
 	if len(result) > 0:
