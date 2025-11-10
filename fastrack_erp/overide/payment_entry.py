058775a86d768c8ab2fb6e1c3ff25ea38c4aaadc
sohan653 <sohanurl653@gmail.com>
2025-11-10 11:37:16 +0000

    fix payment entry issue

--- fastrack_erp/overide/payment_entry.py
+++ fastrack_erp/overide/payment_entry.py
@@ -3,16 +3,13 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 
 
 class CustomPaymentEntry(PaymentEntry):
-	"""
-	Custom Payment Entry class that overrides ERPNext's PaymentEntry
-	This allows you to override any method from the parent class
-	"""
 
 
 	def on_submit(self):
+		frappe.msgprint("Payment Entry Submitted")
 		self.make_gl_entries()
 		self.update_outstanding_amounts()
 		self.update_payment_schedule()
 		self.update_payment_requests()
-		self.update_advance_paid()  # advance_paid_status depends on the payment request amount
+		self.update_advance_paid()  
 		self.set_status()
