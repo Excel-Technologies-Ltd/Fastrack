fe08cb7c7b300f888f4feb84869fe2219966c4cb
excel-azmin <excel.azmin@gmail.com>
2025-12-20 08:03:55 +0000

    add doctype

---
+++ fastrack_erp/fastrack_erp_export/doctype/export_sea_master_bill_test/export_sea_master_bill_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Shaid Azmin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Export Sea Master Bill Test', {
+	// refresh: function(frm) {
+
+	// }
+});
