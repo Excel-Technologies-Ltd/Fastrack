531f6bb688bb4720b98000add0d29d3d266bbd24
Jafor Mia <system5@excelbd.com>
2026-01-15 11:17:50 +0000

    fix: Final design

---
+++ fastrack_erp/fastrack_erp/doctype/port_of_loading/port_of_loading.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Shaid Azmin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Port of Loading', {
+	// refresh: function(frm) {
+
+	// }
+});
